bd8a7d41bd59d1c74c4fc4765cc1e99dbf4aec38
Jacob Hoekert <jshoekert@gmail.com>
2020-02-18 18:03:34 -0600

    back at it

--- apartments.js
+++ apartments.js
@@ -1,19 +1,16 @@
 // Menu
 
 function toggleMobileMenu() {
-
   var menuClasses = document.getElementById("menu").classList;
   var burgerClasses = document.getElementById("burger").classList;
 
-  if (menuClasses.contains("is-active") ) {
-     menuClasses.remove("is-active");
-     burgerClasses.remove("is-active");
+  if (menuClasses.contains("is-active")) {
+    menuClasses.remove("is-active");
+    burgerClasses.remove("is-active");
+  } else {
+    menuClasses.add("is-active");
+    burgerClasses.add("is-active");
   }
-  else {
-     menuClasses.add("is-active");
-     burgerClasses.add("is-active");
-  }
-
 }
 
 // Collapsible
@@ -22,35 +19,37 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-coll[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var content = this.nextElementSibling;
-  if (content.style.maxHeight){
-    content.style.maxHeight = null;
-  } else {
-    content.style.maxHeight = content.scrollHeight + "px";
-  } 
-});
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
 
-// Gallery 
+// Gallery
 
 var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-showDivs(slideIndex += n);
+  showDivs((slideIndex += n));
 }
 
 function showDivs(n) {
-var i;
-var x = document.getElementsByClassName("mySlides");
-if (n > x.length) {slideIndex = 1} 
-if (n < 1) {slideIndex = x.length} ;
-for (i = 0; i < x.length; i++) {
-  x[i].style.display = "none"; 
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
 }
-x[slideIndex-1].style.display = "block"; 
-}
-
-
